43e23ca83d4f64ec5893fb794fd3ebce5aef035c
Roman <leshchuh31@gmail.com>
2022-08-04 18:18:58 +0300

    Зробив вивід при наводженні на слово

--- Task1/script.js
+++ Task1/script.js
@@ -2,25 +2,77 @@
 
 
 function docOnReady(event){
-    console.log(event);
-    loadNames();
     fetchData('./data.json');
+    const CheckList = document.getElementById("Fishing");
+    CheckList.addEventListener("mouseover", showData);
+    buttonClose();
 }
+
 
 async function fetchData(url){
-    console.log("fafsasfas");
-    let response = await fetch(url);
-    let mydata = await response.json();
-    console.log(response);
-    for(let key in mydata){
-      console.log(mydata[key]);
+    const myList = document.querySelector('ul');
+    fetch(url)
+    .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+      })
+    .then((data) => {
+    for (const item of data.items) {
+        for(const value in item)
+        {
+          if(value === 'dataType')
+           continue;
+          const listItem = document.createElement('input');
+          const label = document.createElement('label');
+          const breakP = document.createElement('br');
+
+          listItem.setAttribute("type", "checkbox");
+          listItem.setAttribute("id", `${value}`);
+    
+          label.setAttribute('for',`${value} `)
+          label.innerHTML = `${value}: ${item[value]}`;
+          
+          
+          myList.appendChild(listItem);
+          myList.appendChild(label);
+          myList.appendChild(breakP);
+        } 
+    //   const listItem = document.createElement('input');
+    //   const label = document.createElement('label');
+
+    //   listItem.setAttribute("type", "checkbox");
+    //   listItem.setAttribute("id", `${product.instrument}`);
+
+    //   label.setAttribute('for',`${product.instrument}`)
+    //   label.innerHTML = `${product.instrument}`;
+
+    //   console.log(`${product.instrument}`);
+      
+      
+    //   myList.appendChild(listItem);
+    //   myList.appendChild(label);
     }
+  }).catch(console.error);
+    
+    
 }
 
-async function loadNames() {
-    const response = await fetch('./data.json');
-    const names = await response.json();
-    console.log(names); 
+function buttonClose(){
+    const btn = document.getElementById('close');
+    btn.addEventListener("click", hideData);
 }
+
+function showData(){
+    const box = document.getElementById('CheckList');
+    box.style.visibility = 'visible';
+}
+
+function hideData(){
+    const box = document.getElementById('CheckList');
+    box.style.visibility = 'hidden';
+}
+
 
 window.addEventListener("DOMContentLoaded",docOnReady);
